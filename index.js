956137261a981a1f2064f73073ab9d243835b140
James Ferguson <ncjamesferguson@gmail.com>
2018-05-22 17:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function justInvoke(fn) {
-
+  return fn()
 }
 
 function setThisWithCall(fn, thisValue, arg) {
@@ -7,9 +7,9 @@ function setThisWithCall(fn, thisValue, arg) {
 }
 
 function setThisWithApply(fn, thisValue, args) {
-  
+
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
-  
+
 }
